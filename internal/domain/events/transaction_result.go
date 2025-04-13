16330eff8a510a666cca011276af596b75f05fe6
lucasb <lkab05@hotmail.com>
2025-04-13 18:33:03 -0300

    add PendingTransaction and TransactionResult event types with associated constructors and Kafka producer/consumer implementation

---
+++ internal/domain/events/transaction_result.go
@@ -0,0 +1,20 @@
+package events
+
+import "github.com/lucasbarbosaalves/go-gateway/internal/domain"
+ 
+ 
+ type TransactionResult struct {
+ 	InvoiceID string `json:"invoice_id"`
+ 	Status    string `json:"status"`
+ }
+ 
+ func NewTransactionResult(invoiceID string, status string) *TransactionResult {
+ 	return &TransactionResult{
+ 		InvoiceID: invoiceID,
+ 		Status:    status,
+ 	}
+ }
+ 
+ func (t *TransactionResult) ToDomainStatus() domain.Status {
+ 	return domain.Status(t.Status)
+ }
